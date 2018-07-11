59cd7fe255a8e81c7e74e75ded0ba20dc39a06fc
miguelmassv <miguelmassv@hotmail.com>
2018-07-11 13:48:23 -0500

    crear

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,4 +2,17 @@ class Api::V1::ProductsController < ApplicationController
     def index
       render json: Product.all
     end
+    def create
+      product = Product.new(product_params)
+      if product.save
+        render json: product, status: 201
+      else
+        render json: {errors: product.errors}, status: 422
+      end
+    end
+  
+    private
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end    
   end
